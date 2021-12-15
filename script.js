cf5274ff5a004420ff00a9e5472cfaff9679f961
Gabriel Bastos <gbastosbarbosa@gmail.com>
2021-12-15 16:46:05 -0300

    first comit
    um objeto com array dentro

---
+++ script.js
@@ -0,0 +1,15 @@
+let postagem = {
+    titulo: 'a',
+    mensagem: 'b',
+    autor: 'c',
+    visualizacoes: 17,
+    comentarios: [
+        { autor: 'a', mensagem: 'b' },
+        { autor: 'a', mensagem: 'b' }
+
+    ],
+
+    estaAoVivo: true
+
+}
+console.log(postagem);
